9a0794c09976d986c2bbb960194ed6d2f8d87531
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-03-30 10:14:48 +0530

    Initial default VS project  and introduction text to Observer Pattern.

---
+++ 0002_Observer_Pattern/0002_Observer_Pattern_Example/0002_Observer_Pattern_Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002_Observer_Pattern_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
