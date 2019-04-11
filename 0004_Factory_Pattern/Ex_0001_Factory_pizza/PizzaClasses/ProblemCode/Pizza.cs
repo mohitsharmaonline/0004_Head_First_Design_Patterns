b7befad2a3832444b99d929f2ce809fbc980953f
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-11 23:48:46 +0530

    Description of problem.

---
+++ 0004_Factory_Pattern/Ex_0001_Factory_pizza/PizzaClasses/ProblemCode/Pizza.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaClasses
+{
+    public abstract class Pizza
+    {
+        public void Prepare()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Bake()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Cut()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Box()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ 0004_Factory_Pattern/Ex_0001_Factory_pizza/PizzaClasses/ProblemCode/PizzaShop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaClasses.ProblemCode
+{
+    public class PizzaShop
+    {
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza = null;
+            // Below is the type of code that shall require
+            // OrderPizza method to be modified every time
+            // a new pizza is introduced or old one is removed.
+            if(type == "cheese")
+            {
+                pizza = new CheesePizza();
+            }
+            else if(type == "pepproni")
+            {
+                pizza = new PepproniPizza();
+            }
+            else if (type == "clam")
+            {
+                pizza = new ClamPizza();
+            }
+            else if (type == "veggie")
+            {
+                pizza = new VeggiePizza();
+            }
+
+            // below code will remain same.
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+    }
+}
