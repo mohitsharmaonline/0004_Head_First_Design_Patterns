a9f5cdf6e446de82bc3d14f8df1d8ddcf8cd8f14
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-05-15 20:28:52 +0530

    Defaut changes in VS to add a class library project.

--- 0006_Command_Pattern/Ex_0001_Command_Ptn/Command_ptn_classes/Class1.cs
+++ 0006_Command_Pattern/Ex_0001_Command_Ptn/Command_ptn_classes/Class1.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex_0001_Command_Ptn
+namespace Command_ptn_classes
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
